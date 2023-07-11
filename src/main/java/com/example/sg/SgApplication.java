2af5143b9b210a98d458db13b53f95990451438d
rahul <rahulpatna89@gmail.com>
2023-07-12 01:11:24 +0530

    commit 1

---
+++ src/main/java/com/example/sg/SgApplication.java
@@ -0,0 +1,13 @@
+package com.example.sg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgApplication.class, args);
+	}
+
+}
